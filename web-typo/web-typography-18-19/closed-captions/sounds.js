abdf94ebcd46d9f76ab5584e4478a59e041a48f5
YoussefKassem <youssef.kassem@windesheimflevoland.nl>
2019-06-05 11:51:33 +0200

    Add files via upload

---
+++ web-typo/web-typography-18-19/closed-captions/sounds.js
@@ -0,0 +1,31 @@
+sounds = [
+	0,  //sound 0
+	9,  //sound 1
+	9,  //sound 2
+	15, //sound 3
+	16.5, // alarm 1  - sound 4
+	24, //sound 5
+	25.8, // alarm 2 - sound 6
+	31, //sound 7
+	32,  // alarm 3  - sound 8
+	33,	 // peep sound - sound 9
+	51, // medium peep - sound 10
+	65,	//  hard peep - sound 11
+	75, // peep stops -sound 12
+	88, // heart rate goes faster
+	88.1, //sound 14
+	78, // sound 15
+	86, // sound 16
+	86.1, // sound 17
+	99, // sound 18
+	114, // sound 19
+	130, // sound 20
+	88.2, // sound 21
+	88.1, // sound 22
+	136, // sound 23
+	78, // sound 24
+	86, // sound 25
+	130, // sound 26
+
+
+];
